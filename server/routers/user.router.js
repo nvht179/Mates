253baa8e107088c483d15e96d846c48cd685a9ff
Koa28062004 <lnkhoa28062004@gmail.com>
2024-12-17 20:09:23 +0700

    feat(server): add get user by id

--- server/routers/user.router.js
+++ server/routers/user.router.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const UserController = require("../controllers/user.controller");
 
 router.post("/checkUserByEmail", UserController.checkUserByEmail);
+router.get("/getUserByID/:id", UserController.getUserByID);
 
 module.exports = router;
