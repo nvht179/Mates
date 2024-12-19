17985ba8e697861f1bcdda3de81b760c7b9f39de
NguyenCThong <thong16112004@gmail.com>
2024-12-20 03:32:03 +0700

    GradingPage and GradedPage components

---
+++ client/src/pages/GradedPage.tsx
@@ -0,0 +1,35 @@
+import GradedList from "../components/ToGradeList";
+import Grade from "../interfaces/Grade";
+
+function GradedPage() {
+
+    const grades: Grade[] = [
+        {
+            name: "Dao Xuan Thanh",
+            assignment: "Meow Meow",
+            status: "Submitted",
+            submittedOn: "23:00, 14/12/2024",
+            feedback: "So bad",
+            weight: 2,
+            score: "100/100",
+        },
+        {
+            name: "Dao Xuan Thanh",
+            assignment: "Gau Gau",
+            status: "Submitted",
+            submittedOn: "23:00, 14/12/2024",
+            feedback: "Very goo...",
+            weight: 2,
+            score: "100/100",
+        },
+    ];
+
+    return (
+        <div className="mx-10">
+            <GradedList grades={grades} />
+
+        </div>
+    )
+}
+
+export default GradedPage;
